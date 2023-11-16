4529543c5fd182b30dfef8b66ee492b2b4d6fa3e
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-11-16 16:45:11 -0300

    inicio proyecto

---
+++ registrapp/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login-alumno',
+    loadChildren: () => import('./login-alumno/login-alumno.module').then( m => m.LoginAlumnoPageModule)
+  },
+  {
+    path: 'login-docente',
+    loadChildren: () => import('./login-docente/login-docente.module').then( m => m.LoginDocentePageModule)
+  },
+  {
+    path: 'home-docente',
+    loadChildren: () => import('./home-docente/home-docente.module').then( m => m.HomeDocentePageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'recovery',
+    loadChildren: () => import('./recovery/recovery.module').then( m => m.RecoveryPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ registrapp/src/app/login-docente/login-docente.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-docente',
+  templateUrl: './login-docente.page.html',
+  styleUrls: ['./login-docente.page.scss'],
+})
+export class LoginDocentePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
